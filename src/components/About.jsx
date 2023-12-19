b18c4906abf785b9ec1e721b595bfe75269d711b
Aryainguz <aryaninguz369@gmail.com>
2023-12-19 17:53:47 +0530

    initialized about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer';
 
 const About = () => {
     return (
         <>
             <Header />
             <h1>About</h1>
+            <Footer />
         </>
     )
 }
